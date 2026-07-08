92046457a404fedcfed17942e6add310ce597090
omshantisahu252-jpg <omshantisahu252@gmail.com>
2026-07-08 12:14:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-    WORKER_URL: 'https://your-worker-name.yourusername.workers.dev', // Update after deployment
+    WORKER_URL: 'https://background-remover.omshantisahu252.workers.dev/', // Update after deployment
     MAX_FILE_SIZE: 10 * 1024 * 1024, // 10MB
     SUPPORTED_FORMATS: ['image/jpeg', 'image/png', 'image/webp']
 };
